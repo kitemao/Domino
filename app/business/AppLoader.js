7d072eaaf4ad8ab20b71fa7984ed7e06e7979888
miaojian <kitemao@gmail.com>
2014-03-28 19:23:23 +0800

    travis test

--- app/business/AppLoader.js
+++ app/business/AppLoader.js
@@ -1,17 +1,18 @@
 require(['RequireConfig', 'app'], function () {
-    
+
     var angular = require('angular');
+    console.info(window.location.search.indexOf('e2e') !== -1);
 
     // 如果search包含e2e，则为e2e测试
-    if (window.location.search.indexOf('e2e') !== -1) {
-        
-        require(['/e2e-mocks.js'], function () {
-            
-            angular.bootstrap(document, ['domino']);
-        });
-    }
-    else {
-        
+    // if (window.location.search.indexOf('e2e') !== -1) {
+
+    //     require(['/e2e-mocks.js'], function () {
+
+    //         angular.bootstrap(document, ['domino']);
+    //     });
+    // }
+    // else {
+
         angular.bootstrap(document, ['domino']);
-    }
+    // }
 });
